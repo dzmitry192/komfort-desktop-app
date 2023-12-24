073310d659bee9558e3df6719c5048190b13387f
Daniil <Danik1320@yandex.by>
2023-12-24 21:52:40 +0300

    custom alert styles

--- src/main/java/com/example/project_for_university/utils/AlertUtil.java
+++ src/main/java/com/example/project_for_university/utils/AlertUtil.java
@@ -4,6 +4,7 @@ import javafx.event.Event;
 import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.DialogPane;
 import javafx.stage.Stage;
 
 import java.util.Optional;
@@ -14,8 +15,21 @@ public class AlertUtil {
         alert.setTitle("Предупреждение");
         alert.setHeaderText(headerText);
         alert.setContentText(contentText);
-
         alert.initOwner(stage);
+
+
+        DialogPane dialogPane = alert.getDialogPane();
+
+        ButtonType btn_ok = new ButtonType("Ок");
+
+        dialogPane.getButtonTypes().clear();
+        dialogPane.getButtonTypes().add(btn_ok);
+
+        dialogPane.lookupButton(btn_ok).getStyleClass().add("btn-blue-filled");
+        dialogPane.getStyleClass().add("alert-dialog");
+
+        dialogPane.getStylesheets().add(AlertUtil.class.getResource("/com/example/project_for_university/styles/shared.css").toExternalForm());
+
 
         alert.showAndWait();
         stage.show();
@@ -26,12 +40,27 @@ public class AlertUtil {
         alert.setTitle("Подтверждение");
         alert.setHeaderText(headerText);
         alert.setContentText(contentText);
-
         alert.initOwner(stage);
+
+
+        DialogPane dialogPane = alert.getDialogPane();
+
+        ButtonType btn_cancel = new ButtonType("Отменить");
+        ButtonType btn_ok = new ButtonType("Ок");
+
+        dialogPane.getButtonTypes().clear();
+        dialogPane.getButtonTypes().addAll(btn_cancel, btn_ok);
+
+        dialogPane.lookupButton(btn_cancel).getStyleClass().add("btn-red-filled");
+        dialogPane.lookupButton(btn_ok).getStyleClass().add("btn-blue-filled");
+        dialogPane.getStyleClass().add("alert-dialog");
+
+        dialogPane.getStylesheets().add(AlertUtil.class.getResource("/com/example/project_for_university/styles/shared.css").toExternalForm());
+
 
         Optional<ButtonType> result = alert.showAndWait();
         stage.show();
 
-        return result.isPresent() && result.get() == ButtonType.OK;
+        return result.isPresent() && result.get() == btn_ok;
     }
 }
