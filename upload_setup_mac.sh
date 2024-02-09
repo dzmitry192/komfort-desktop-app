0d760a8c9ef22da9d120bb15434253364a01e51c
Daniil_Rummo <Danik1320@yandex.by>
2024-02-10 00:02:20 +0300

    test upload file to server

--- upload_setup_mac.sh
+++ upload_setup_mac.sh
@@ -2,9 +2,8 @@
 
 filename='/Users/daniil/Downloads/some.dmg'
 filetype='application/octet-stream'
-token='YWRtaW5AeWFuZGV4LmJ5OmFkbWluRGx5YURpbWtpOTE4Ml9nYXk='
 url='http://localhost/desktop/mac/1.0'
 
 curl "$url" \
   --form "setup=@$filename;type=$filetype" \
-  -H "Authorization: Basic $token"
+  -H "Authorization: Basic werewrew"
--- upload_setup_win.bat
+++ upload_setup_win.bat
@@ -2,7 +2,6 @@
 
 @REM SET "filename=\Users\daniil\Downloads\some.dmg"
 SET "filetype=application\octet-stream"
-SET "token=YWRtaW5AeWFuZGV4LmJ5OmFkbWluRGx5YURpbWtpOTE4Ml9nYXk"
 SET "url=http://localhost/desktop/mac/1.0"
 
-curl "%url%" "--form" "setup=@%1;type=%filetype%" "-H" "Authorization: Basic %token%"
+curl "%url%" "--form" "setup=@%1;type=%filetype%" "-H" "Authorization: Basic qweqwew"
